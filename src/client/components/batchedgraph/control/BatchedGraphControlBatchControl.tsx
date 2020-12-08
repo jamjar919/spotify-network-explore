d7160785fc30ce052d2ee4a3313bbfbf185e845f
James Paterson <jpaterson@tripadvisor.com>
2020-12-08 00:08:01 +0000

    play/pause controls

--- src/client/components/batchedgraph/control/BatchedGraphControlBatchControl.tsx
+++ src/client/components/batchedgraph/control/BatchedGraphControlBatchControl.tsx
@@ -1,19 +1,25 @@
 import React, {FunctionComponent} from "react";
+import Play from "../../../svg/play.svg";
+import Pause from "../../../svg/pause.svg";
+import Prev from "../../../svg/prev.svg";
+import Next from "../../../svg/next.svg";
 
 const BatchedGraphControlBatchControl: FunctionComponent<{}> = () => {
+    const isPlaying = false;
+
     return (
         <div className="batch-controller">
-            <div className="batch-controller-previous">
-                Prev
-            </div>
-            <div className="batch-controller-play">
-                Play
-            </div>
-            <div className="batch-controller-next">
-                Next
-            </div>
+            <button className="batch-controller-previous">
+                <Prev />
+            </button>
+            <button className="batch-controller-play-pause">
+                {isPlaying ? <Pause /> : <Play />}
+            </button>
+            <button className="batch-controller-next">
+                <Next />
+            </button>
         </div>
     );
-}
+};
 
 export default BatchedGraphControlBatchControl;
