a757f9f55d5030cd26bf9afa7e73896dd377f083
James Paterson <jpaterson@tripadvisor.com>
2021-01-18 23:05:07 +0000

    prettify login page

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import Logo from "../svg/logo-white.svg";
 
 const Login = () => (
     <div className="login-container">
-        <div className="login">
-            <h1>Spotify Network Explorer</h1>
-            <p>This a simple app that shows you a visual representation of your playlists.</p>
-            <a href={"/spotify/login"}>Log in with Spotify</a>
+        <div className="login-content">
+            <div className="login-left">
+                <div className="login-cta">
+                    <div className="login-logo">
+                        <Logo height="128px" width="128px" />
+                    </div>
+                    <h1>Genre Explorer for Spotify</h1>
+                    <p>You'll need to log in with Spotify to get started.</p>
+                    <a href={"/spotify/login"} className="login-link">Log in with Spotify</a>
+                </div>
+            </div>
+            <div className="login-demo">
+                // demo here
+            </div>
+        </div>
+        <div className="login-footer">
+            <div className="login-footer-content">
+                Created by <a href="http://thejamespaterson.com">James Paterson</a> •
+                Fork on <a href="https://github.com/jamjar919/spotify-network-explore">Github</a> •
+                Not endorsed or supported by <a href="https://www.spotify.com/">Spotify</a>
+            </div>
         </div>
     </div>
 );
--- src/client/components/overlayui/OverlayBase.tsx
+++ src/client/components/overlayui/OverlayBase.tsx
@@ -1,7 +1,7 @@
 import React, {FunctionComponent} from "react";
 
 
-export const OverlayBase: FunctionComponent<void>  = ({ children}) => {
+export const OverlayBase: FunctionComponent<{}>  = ({ children}) => {
     return (
         <div className="overlay-base">
             {children}
