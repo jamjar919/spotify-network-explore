797e95acc4df27b31276169bf1cbcf6f83c454b5
James Paterson <jpaterson@tripadvisor.com>
2021-01-19 00:28:30 +0000

    weird genre selector

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -10,6 +10,11 @@ const Login = () => (
                         <Logo height="128px" width="128px" />
                     </div>
                     <h1>Genre Explorer for Spotify</h1>
+                    <p>
+                        Genre Explorer uses your playlist and listening data to create a custom visualisation of the musical genres
+                        that you listen to on Spotify. Ever wondered what <strong>{getWeirdGenre()}</strong> or <strong>{getWeirdGenre()}</strong> sound like,
+                        or why they showed up on your Spotify discover?
+                    </p>
                     <p>You'll need to log in with Spotify to get started.</p>
                     <a href={"/spotify/login"} className="login-link">Log in with Spotify</a>
                 </div>
@@ -27,5 +32,33 @@ const Login = () => (
         </div>
     </div>
 );
+
+const getWeirdGenre = (): string => {
+    const genres = [
+        "escape room",
+        "microhouse",
+        "swirl psych",
+        "wonky",
+        "vegan straight edge",
+        "shimmer pop",
+        "nerdcore",
+        "martial industrial",
+        "lowercase",
+        "freakbeat",
+        "deep psychobilly",
+        "destroy techno",
+        "black sludge",
+        "brostep",
+        "abstracto",
+        "gauze pop",
+        "jerk",
+        "stomp and flutter",
+        "shiver pop",
+        "skwee",
+        "unblack metal"
+    ];
+
+    return genres[Math.floor(Math.random() * genres.length)];
+};
 
 export default Login;
