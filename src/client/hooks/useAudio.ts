c926300bec39821076b9bc3cee53bc1b7035cd16
James Paterson <jpaterson@tripadvisor.com>
2021-04-03 01:47:44 +0100

    comment

--- src/client/hooks/useAudio.ts
+++ src/client/hooks/useAudio.ts
@@ -2,6 +2,13 @@
 // Global audio element
 let audio: HTMLAudioElement = new Audio();
 
+/**
+ * This hook lets you use a single global audio element, ensuring that multiple songs do not play at the same time. The
+ * src is passed through to the onPlay and onPause callbacks to allow the state to be accurately updated.
+ *
+ * @param onPlay    Triggered every time the element is played
+ * @param onPause   Triggered every time the element is paused
+ */
 export const useAudio = (
     onPlay?: (src: string) => void,
     onPause?: (src: string) => void
