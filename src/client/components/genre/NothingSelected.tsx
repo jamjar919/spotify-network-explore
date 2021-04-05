df0edd7e45210e606362b8bbcdd4d5e97bc93aa4
James Paterson <jpaterson@tripadvisor.com>
2021-04-05 15:26:39 +0100

    random

--- src/client/components/genre/NothingSelected.tsx
+++ src/client/components/genre/NothingSelected.tsx
@@ -5,9 +5,13 @@ export const NothingSelected = () => {
 
     return (
         <div className="genre-selector">
-            <h2>Nothing Selected</h2>
+            <h2 className="genre-title">
+                <div>Nothing Selected</div>
+                <div className="inline-random-genre-button">
+                    <RandomGenreButton/>
+                </div>
+            </h2>
             <p>Click on a genre to display information about it, or select one below</p>
-            <h2>Random: <RandomGenreButton/></h2>
         </div>
     );
 };
